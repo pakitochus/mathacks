eb8bf5d31066e83218717adaaebac1c93381a130
Francisco Jesús Martínez Murcia <fjesusmartinez@ugr.es>
2015-05-18 11:45:05 +0200

    Corrected weighting function in SWPCA

--- swpca.m
+++ swpca.m
@@ -141,14 +141,14 @@ function y = weighting(p,d,k)
 sumatoria = 1;
 for jarl=1:length(d),
     if ndims(p)==1
-        sumatoria = sumatoria + power(-1,1-d(jarl))*exp(-squeeze(abs(p(jarl)))/k);
+        sumatoria = sumatoria + d(jarl)*exp(-squeeze(abs(p(jarl)))/k);
     elseif ismatrix(p)
-        sumatoria = sumatoria + power(-1,1-d(jarl))*exp(-squeeze(abs(p(jarl,:)))/k);
+        sumatoria = sumatoria + d(jarl)*exp(-squeeze(abs(p(jarl,:)))/k);
     elseif ndims(p)==3
-        sumatoria = sumatoria + power(-1,1-d(jarl))*exp(-squeeze(abs(p(jarl,:,:)))/k);
+        sumatoria = sumatoria + d(jarl)*exp(-squeeze(abs(p(jarl,:,:)))/k);
     else
         error('P matrices with dimensions > 3 not supported');
     end
 end
-y = sumatoria/2;
+y = sumatoria/length(d);
 end
